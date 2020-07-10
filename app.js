71a4baa5710901cf0d1c9160ad503b87a776b9be
yoohooitstoo <stumanscott@gmail.com>
2020-07-09 20:19:28 -0400

    view roles finished

--- app.js
+++ app.js
@@ -62,30 +62,23 @@ function start() {
         "exit",
       ],
     })
-    .then(function ({action}) {
+    .then(function ({ action }) {
       // based on their answer, either call the bid or the post functions
       if (action === "View All Employees") {
         viewEmployees();
-      }
-      else if (action === "View All Employees By Department") {
+      } else if (action === "View All Employees By Department") {
         viewDepartment();
-      }
-      else if (action === "View Employees By Manager") {
+      } else if (action === "View All Employees By Manager") {
         viewManager();
-      }
-      else if (action === "Add Employee") {
+      } else if (action === "Add Employee") {
         addEmployee();
-      }
-      else if (action === "Remove Employee") {
+      } else if (action === "Remove Employee") {
         removeEmployee();
-      }
-      else if (action === "Update Employee Role") {
+      } else if (action === "Update Employee Role") {
         updateRole();
-      }
-      else if (action === "Update Employee Manager") {
+      } else if (action === "Update Employee Manager") {
         updateManager();
-      }
-      else if (action === "View All Roles") {
+      } else if (action === "View All Roles") {
         viewRoles();
       } else {
         exit();
@@ -94,33 +87,36 @@ function start() {
 }
 
 const viewEmployees = () => {
-    const query = "SELECT employee.id, employee.first_name, employee.last_name, department.department_name, role.salary, manager.first_name AS manager_first, manager.last_name AS manager_last FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;";
-  connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-    } if (err) throw err;
+  const query =
+    "SELECT employee.id, employee.first_name, employee.last_name, department.department_name, role.salary, manager.first_name AS manager_first, manager.last_name AS manager_last FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;";
+  connection.query(query, function (err, res) {
+    for (var i = 0; i < res.length; i++) {}
+    if (err) throw err;
     console.table(res);
-  start();
-});
+    start();
+  });
 };
 
 const viewDepartment = () => {
-    const query = "SELECT department.id, department.department_name, employee.first_name, employee.last_name, role.title FROM department LEFT JOIN employee ON department.id = employee.role_id LEFT JOIN role ON department.id = role.department_id;"
-    connection.query(query, function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-      } if (err) throw err;
-      console.table(res);
+  const query =
+    "SELECT department.id, department.department_name, employee.first_name, employee.last_name, role.title FROM department LEFT JOIN employee ON department.id = employee.role_id LEFT JOIN role ON department.id = role.department_id;";
+  connection.query(query, function (err, res) {
+    for (var i = 0; i < res.length; i++) {}
+    if (err) throw err;
+    console.table(res);
     start();
-    });
+  });
 };
 
 const viewManager = () => {
-    const query = "SELECT department.id, department.department_name, employee.first_name, employee.last_name, role.title FROM department LEFT JOIN employee ON department.id = employee.role_id LEFT JOIN role ON department.id = role.department_id;"
-    connection.query(query, function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-      } if (err) throw err;
-      console.table(res);
+  const query =
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name, employee.manager_id, manager.first_name AS manager_first, manager.last_name AS manager_last FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;";
+  connection.query(query, function (err, res) {
+    for (var i = 0; i < res.length; i++) {}
+    if (err) throw err;
+    console.table(res);
     start();
-    });
+  });
 };
 
 const addEmployee = () => {
@@ -144,9 +140,16 @@ const updateManager = () => {
 };
 
 const viewRoles = () => {
-  console.log("save me");
-  start();
+    const query =
+    "SELECT role.title, role.salary, role.department_id, department.department_name FROM role LEFT JOIN department ON role.department_id = department.id;";
+  connection.query(query, function (err, res) {
+    for (var i = 0; i < res.length; i++) {}
+    if (err) throw err;
+    console.table(res);
+    start();
+  });
 };
+  
 
 const exit = () => {
   console.log("you exited");
